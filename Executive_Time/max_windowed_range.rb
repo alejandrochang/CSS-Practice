748f30f54a961dc38fc3abee6d88a7f8f51b31cd
Nathan Harris <nathanharris.sf@gmail.com>
2018-08-30 17:32:37 -0700

    Added max_window_range file

---
+++ Executive_Time/max_windowed_range.rb
@@ -0,0 +1,14 @@
+
+def windowed_max_range(arr, target)
+  current_max_range = nil
+  min = 0
+  max = 0
+  arr.each_with_index do |num|
+
+  end
+end
+
+p windowed_max_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
+p windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
+p windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
+p windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
